74699bc0b760deed10d6edbb42ab17c8215c14fe
Jeff Forcier <jeff@bitprophet.org>
2023-12-01 22:06:13 -0500

    Refactor in prep for tests

--- src/exercises/day1.rs
+++ src/exercises/day1.rs
@@ -26,49 +26,48 @@ fn part1() -> usize {
     tally
 }
 
+fn line_to_digits(line: &str) -> Vec<&str> {
+    let pattern = regex::Regex::new(r"one|two|three|four|five|six|seven|eight|nine|[0-9]").unwrap();
+    pattern
+        .find_iter(line)
+        .map(|y| {
+            let digit = y.as_str();
+            match digit.len() {
+                1 => digit,
+                _ => match digit {
+                    "one" => "1",
+                    "two" => "2",
+                    "three" => "3",
+                    "four" => "4",
+                    "five" => "5",
+                    "six" => "6",
+                    "seven" => "7",
+                    "eight" => "8",
+                    "nine" => "9",
+                    _ => panic!("wtf? {:?} shouldn't have matched the regex!", digit),
+                },
+            }
+        })
+        .collect()
+}
+
+fn digits_to_number(digits: Vec<&str>) -> usize {
+    let firstlast: String = vec![digits.first(), digits.last()]
+        .into_iter()
+        .map(|x| *x.unwrap())
+        .collect();
+    firstlast.parse::<usize>().unwrap()
+}
+
 fn part2() -> usize {
     let mut tally = 0;
     let file = fs::read_to_string("inputs/day1.txt").unwrap();
-    let pattern =
-        regex::Regex::new(r"one|two|three|four|five|six|seven|eight|nine|[0-9]").unwrap();
-    let parsed: Vec<_> = file
-        .lines()
-        .map(|x| {
-            let matches: Vec<_> = pattern
-                .find_iter(x)
-                .map(|y| {
-                    let digit = y.as_str();
-                    match digit.len() {
-                        1 => digit,
-                        _ => match digit {
-                            "one" => "1",
-                            "two" => "2",
-                            "three" => "3",
-                            "four" => "4",
-                            "five" => "5",
-                            "six" => "6",
-                            "seven" => "7",
-                            "eight" => "8",
-                            "nine" => "9",
-                            _ => panic!("wtf? {:?} shouldn't have matched the regex!", digit),
-                        },
-                    }
-                })
-                .collect();
-            (x, matches)
-        })
-        .collect();
+    let parsed: Vec<_> = file.lines().map(|x| (x, line_to_digits(x))).collect();
     for (string, digits) in parsed {
-        let firstlast: String = vec![digits.first(), digits.last()]
-            .into_iter()
-            .map(|x| *x.unwrap())
-            .collect();
-        let caliban = firstlast.parse::<usize>().unwrap();
-        println!(
-            "{:?} => {:?} => {:?} => {:?}",
-            string, digits, firstlast, caliban
-        );
-        tally += caliban;
+        print!("{:?} => {:?}", string, digits);
+        let number = digits_to_number(digits);
+        println!(" => {:?}", number);
+        tally += number;
     }
     tally
 }
