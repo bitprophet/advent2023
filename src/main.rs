7ad854ae8994acaf84b0a252206e1babfe12653a
Jeff Forcier <jeff@bitprophet.org>
2023-12-02 18:23:27 -0500

    rust 1.73 toolchain = new lint re: pointless vec! use

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 mod exercises;
 
 fn main() {
-    let days = vec![exercises::day1::go];
+    let days = [exercises::day1::go];
     let mut args = env::args();
     let day = match args.nth(1) {
         Some(x) => x.parse().unwrap(),
