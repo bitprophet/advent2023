beef41320763bea67addd27f6af0c912e95bf514
Jeff Forcier <jeff@bitprophet.org>
2023-12-03 14:35:52 -0500

    Allow selecting just a day's tests when watch'ing

--- tasks.py
+++ tasks.py
@@ -2,11 +2,14 @@ from invoke import task
 
 
 @task
-def watch(c):
+def watch(c, tests=None):
     """
     Cargo watch oneshot (lint/test/run).
+
+    :param str tests: If given, added as parameter to the 'test' subcall.
     """
-    c.run(
-        "cargo watch --clear -x 'clippy -- --deny warnings' -x test -x aoc",
-        pty=True,
-    )
+    base = "cargo watch --clear"
+    clippy = "-x 'clippy -- --deny warnings'"
+    test = f"-x 'test {tests or ''}'"
+    aoc = "-x aoc"
+    c.run(f"{base} {clippy} {test} {aoc}", pty=True)
