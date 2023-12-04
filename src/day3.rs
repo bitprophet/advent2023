f55f9dea03ace8682eb3113f077ec54d703ca94d
Jeff Forcier <jeff@bitprophet.org>
2023-12-03 19:07:50 -0500

    Checkpoint: rejiggered stuff, partway into part number checking

--- src/day3.rs
+++ src/day3.rs
@@ -1,11 +1,29 @@
 use aoc_runner_derive::aoc;
 
+// TODO: gotta be something more compact lol
+const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// NOTE: doing things (y, x) style because brain goes row->col? meh
+
 // NOTE: there are plentiful grid libs but so far trying to save time has
 // mostly screwed me up, so let's try doing this regular for now
 
 // NOTE: trying to essentially wrap a Vec seems to get hairy fast re: weird
 // peculiarities of things like impl Index. Not worth it yet...
 type Row = Vec<char>;
+type Cell = char;
+
+#[derive(Debug)]
+struct Point {
+    y: usize,
+    x: usize,
+}
+
+#[derive(Debug)]
+struct Number {
+    start: Point,
+    chars: Vec<char>,
+}
 
 #[derive(Debug)]
 struct Schematic {
@@ -27,6 +45,59 @@ impl Schematic {
         }
         output
     }
+
+    fn get(&self, y: usize, x: usize) -> Cell {
+        self.rows[y][x]
+    }
+
+    fn number_is_label(&self, number: &Number) -> bool {
+        // TODO: replaceme
+        number.chars.is_empty()
+    }
+
+    fn sum_part_numbers(&self) -> usize {
+        let mut sum = 0;
+        // TODO: feels like this 'wants' to be an Option<Number> but I'm not
+        // clear on how to modify the internal value w/o a lot of vexing
+        // full-object recreation (eg option.replace(...))
+        // So, start with a bogus number and set a flag to false.
+        let mut cur = Number {
+            start: Point { y: 0, x: 0 },
+            chars: Vec::new(),
+        };
+        let mut active = false;
+        for (y, row) in self.rows.iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                // Numberriffic
+                if DIGITS.contains(cell) {
+                    // Starting a new number.
+                    if !active {
+                        cur = Number {
+                            start: Point { y, x },
+                            chars: vec![*cell],
+                        };
+                        active = true;
+                    // Continuing the current number.
+                    } else {
+                        cur.chars.push(*cell);
+                    }
+                // Not numberriffic: did we just finish a number?
+                } else if active {
+                    active = false;
+                    // And was that number seemingly adjacent to any 'parts'?
+                    if self.number_is_label(&cur) {
+                        sum += cur
+                            .chars
+                            .iter()
+                            .collect::<String>()
+                            .parse::<usize>()
+                            .unwrap();
+                    }
+                }
+            }
+        }
+        sum
+    }
 }
 
 impl From<&str> for Schematic {
@@ -43,8 +114,8 @@ impl From<&str> for Schematic {
 #[aoc(day3, part1)]
 fn schemattic(input: &str) -> usize {
     let schematic = Schematic::from(input);
-    println!("{}", schematic.show());
-    schematic.size()
+    dbg!(schematic.show());
+    schematic.sum_part_numbers()
 }
 
 #[cfg(test)]
@@ -70,7 +141,8 @@ mod tests {
         let s = Schematic::from(sample);
         assert_eq!(s.rows.len(), 10);
         assert_eq!(s.rows[0].len(), 10);
-        assert_eq!(s.rows[0][0], '4');
+        assert_eq!(s.rows[0][2], '7');
+        assert_eq!(s.get(0, 2), '7');
     }
 
     #[test]
